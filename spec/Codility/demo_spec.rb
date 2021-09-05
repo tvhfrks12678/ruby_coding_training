a6853f2fec9feb1b14fc2703d4394fce39050eef
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-09-05 16:49:51 +0900

    test: add codility demo spec

--- spec/Codility/demo_spec.rb
+++ spec/Codility/demo_spec.rb
@@ -11,17 +11,19 @@ describe 'demo' do
 	problems.push({input: [1], answer: 2})
 	problems.push({input: [1,2,3,4,5,6,7,8,9,10,11,12,20,21,-1], answer: 13})
 	problems.push({input: [1,2,3,20,21,-1], answer: 4})
-	problems.push({input: [1000000, -1000000], answer: 1000001})
-	problems.push({input: [999999,1000000], answer: 1000001})
 	problems.push({input: [-1, 0, 1000000], answer: 1})
 	problems.push({input: [-1, 0, 1, 3, 2,1000000], answer: 4})
 	problems.push({input: [*(-1000000..1000000)].shuffle, answer: 1000001})
+	problems.push({input: [-1, 0, -3, 2, 3 ], answer: 1})
+	problems.push({input: [-1, 0, 1, 2, 3, 5], answer: 4})
+	problems.push({input: [1000000, -1000000], answer: 1})
+	problems.push({input: [999999,1000000], answer: 1})
 
 	problems.each do |problem|
 		input = problem[:input]
 		answer = problem[:answer]
 		if input.length > 20
-			context "input 最小:#{input.min} 最大:#{input.max}" do
+			context "input start:#{input.min} end:#{input.max}" do
 				it "return #{answer}" do
 					expect(solution(input)).to  eq answer
 				end
